fe6e60072c2676721fdf3ba3399a0c961f22683e
kfragkedaki <fr-claire@hotmail.com>
2021-12-20 00:52:54 +0100

    add task_2 v1

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,14 +1,14 @@
-from typing import Union, List, Tuple, Dict, Set, Callable
+from typing import Union, List
 from BayesNet import BayesNet
 from enum import Enum
+import helper_functions as helpers
 import pandas as pd
 import copy
-import json
-import os
+
 
 class Heuristics(Enum):
     MIN_ORDER='MIN_ORDER'
-    MIN_FILL='MIN_ORDER'
+    MIN_FILL='MIN_FILL'
     RANDOM='RANDOM'
 
 class BNReasoner:
@@ -95,7 +95,7 @@ class BNReasoner:
 
         return temp_bn.marginal_distrib(Q, e, pi) # type: ignore
 
-    def map_and_mpe(self, order_function: Heuristics, e: List[tuple[str, bool]], M: List[str]=[]): # TODO: add types
+    def map_and_mpe(self, order_function: Heuristics, e: List[tuple[str, bool]], M: List[str]=[]) -> pd.DataFrame:
         """
         Computes the most likely instantiations of M
         given possibly empty set of query variables M and an evidence E
@@ -104,8 +104,9 @@ class BNReasoner:
         :param M: query variables
         :param E: evidence
 
-        :return: TODO
+        :return: Dataframe with most likely instantiations and probability
         """
+        print(order_function)
         temp_bn = copy.deepcopy(self.bn)
 
         if Heuristics.MIN_FILL == order_function: order=temp_bn.min_fill
@@ -114,43 +115,79 @@ class BNReasoner:
 
         return temp_bn.map_and_mpe(order, e, M)
 
-    def create_random_bn(self) -> None:
-        temp_bn = copy.deepcopy(self.bn)
+# def task_2() -> None:
+#     directory = "./test_data/task_2"
 
-        return temp_bn.generate_random(6)
+#     for filename in os.listdir(directory):
+#         if filename.endswith("101.xml"): 
+#             # print(os.path.join(directory, filename))
+#             file = os.path.join(directory, filename)
+#             bn =  BNReasoner(file)
+#             print(file)
+#             print(bn.map_and_mpe(order_function=Heuristics.MIN_ORDER, e=[('node1', False), ('node10', True)]))
+#             continue
+#         else:
+#             continue
 
 def task_2() -> None:
-    directory = "./test_data/task_2"
+    start_num_nodes = 10
+    start_num_roots = 2
+    start_num_min_edges = 2
+    start_num_max_edges = 4
+    increase_nodes = 10
+    num_tries = 5
 
-    for filename in os.listdir(directory):
-        if filename.endswith("101.xml"): 
-            # print(os.path.join(directory, filename))
-            file = os.path.join(directory, filename)
-            bn =  BNReasoner(file)
-            print(file)
-            print(bn.map_and_mpe(order_function=Heuristics.MIN_ORDER, e=[('node1', False), ('node10', True)]))
-            continue
-        else:
-            continue
-            
+    # change variables
+    for run_number in range(num_tries):
+        bn = BayesNet()
+        result = bn.generate_random(number=start_num_nodes, 
+            n_roots=start_num_roots, max_edges=start_num_max_edges, min_edges=start_num_min_edges)
+        bn.draw_structure_sample()
+        print(bn.get_all_cpts())
+        reasoner = BNReasoner(bn)
+
+        start_num_nodes+=increase_nodes
+        for method in Heuristics:
+            print(method)
+            reasoner.map_and_mpe(order_function=method, e=[('node1', False), ('node2', False)], M=['node3', 'node4'])
+            helpers.save_data(method.value, run_number, result)
+
+    # bn = BayesNet()
+    # bn.generate_random(5, n_roots=2)
+    # bn.draw_structure_sample()
+    # int_grah = bn.get_interaction_graph()
+    # bn.draw_graph(int_grah)
+    # print(bn.get_all_cpts())
+    # bn_2 = BayesNet()
+    # bn_2.generate_random()
+    # bn_2.draw_structure_sample()
+    # print(bn_2.get_all_cpts())
+    # bn_3 = BayesNet()
+    # bn_3.generate_random(20, n_roots=4)
+    # bn_3.draw_structure_sample()
+    # print(bn_3.get_all_cpts())
+
+def task_3() -> None:
+    # draw graph
+    bn = BayesNet()
+    bn.load_from_bifxml("testing/diseases.BIFXML")
+    bn.draw_structure()
+
+    # draw interaction graph
+    interaction_graph = bn.get_interaction_graph()
+    bn.draw_graph(interaction_graph)
+
+    disease_problem = BNReasoner('testing/diseases.BIFXML')       
+
+    # priori marginal query.
+    # posterior marginal query.
+    # MAP query
+    # MEP query
+    disease_problem.map_and_mpe(order_function=Heuristics.MIN_ORDER, e=[('gender', False), ('job', True)])
 
 if __name__ =='__main__':
-    bn = BayesNet()
-    bn.generate_random(5, n_roots=2, degree=3)
-    bn.draw_structure_sample()
-    int_grah = bn.get_interaction_graph()
-    bn.draw_graph(int_grah)
-    print(bn.get_all_cpts())
-    bn_2 = BayesNet()
-    bn_2.generate_random()
-    bn_2.draw_structure_sample()
-    print(bn_2.get_all_cpts())
-    bn_3 = BayesNet()
-    bn_3.generate_random(20, n_roots=4, degree=3)
-    bn_3.draw_structure_sample()
-    print(bn_3.get_all_cpts())
 
-    # task_2()
+    task_2()
 
     # lecture_Example = BNReasoner("testing/lecture_Example.BIFXML")
     # lecture_Example_2 = BNReasoner("testing/lecture_Example2.BIFXML")
@@ -161,78 +198,20 @@ if __name__ =='__main__':
     # dog_problem.network_prune(['family-out'],[('dog-out', True), ('hear-bark', False)])
     # print(lecture_Example.marginal_dist(['Slippery Road?',  'Wet Grass?'], [('Winter?', True), ('Sprinkler?', False)], ['Winter?', 'Rain?', 'Sprinkler?']))
     # print(lecture_Example_2.map_and_mpe(order_function=Heuristics.MIN_ORDER, e=[('O', True)], M=['I',  'J']))
-    # print(lecture_Example_2.map_and_mpe(order_function=Heuristics.MIN_ORDER, e=[('O', False), ('J', True)]))
 
-    # # create graphs
-    # # bn = BayesNet()
-    # # bn.load_from_bifxml("testing\\dog_problem.BIFXML")
-    # # interaction_graph = bn.get_interaction_graph()
-    # # bn.draw_structure()
-    # # bn.draw_graph(interaction_graph)
-    # # print(nx.d_separated(bn.structure, {'family-out'}, {'hear-bark'}, {'dog-out'}))
+    # print(lecture_Example_2.map_and_mpe(order_function=Heuristics.MIN_ORDER, e=[('O', False), ('J', False)]))
+    # print(lecture_Example_2.map_and_mpe(order_function=Heuristics.MIN_FILL, e=[('O', False), ('J', False)]))
+    # print(lecture_Example_2.map_and_mpe(order_function=Heuristics.RANDOM, e=[('O', False), ('J', False)]))
 
+    # print(lecture_Example.map_and_mpe(order_function=Heuristics.MIN_ORDER, e=[('Winter?', True), ('Sprinkler?', False)]))
+    # print(lecture_Example.map_and_mpe(order_function=Heuristics.MIN_FILL, e=[('Winter?', True), ('Sprinkler?', False)]))
+    # print(lecture_Example.map_and_mpe(order_function=Heuristics.RANDOM, e=[('Winter?', True), ('Sprinkler?', False)]))
+
+    # print(lecture_Example_2.map_and_mpe(order_function=Heuristics.MIN_ORDER, e=[('O', False), ('J', False)], M=['I',  'Y']))
+    # print(lecture_Example_2.map_and_mpe(order_function=Heuristics.MIN_FILL, e=[('O', False), ('J', False)], M=['I',  'Y']))
+    # print(lecture_Example_2.map_and_mpe(order_function=Heuristics.RANDOM, e=[('O', False), ('J', False)], M=['I', 'Y']))
     # bn = BayesNet()
-    # bn.load_from_bifxml("testing/diseases.BIFXML")
-    # bn.draw_structure()
-    # # disease_problem = BNReasoner('testing/diseases.BIFXML')
-    
-    # # TESTING -------------------------
+    # bn.load_from_bifxml("testing/lecture_Example2.BIFXML")
+    # interaction_graph = bn.get_interaction_graph()
+    # bn.draw_graph(interaction_graph)
 
-    # test_problems = ['dog_problem', 'lecture_example', 'lecture_example2']
-    # # test_problems = ['lecture_example2']
-
-    # # delta value for accuracy in floating point outputs, i.e. the output will have to be within +/- one delta of the desired result
-    # DELTA = 0.01
-
-    # for prob in test_problems:
-    #     # create a reasoner object based on the problem BIFXML file
-    #     reasoner = BNReasoner('testing\\' + prob + '.BIFXML')
-
-    #     # get test data from json
-    #     data = {}
-    #     with open('test_data\\' + prob + '.json', 'r') as infile:
-    #         data = json.load(infile)
-
-    #     # test d-seperation
-    #     for X, Y, Z, result in data['d_sep']:
-    #         assert reasoner.d_seperation(X, Y, Z) == result
-
-    #     # test min-degree ordering
-    #     for X, result in data['min_degree']:
-    #         assert reasoner.ordering_min_degree(X) == result
-
-    #     # test min-fill ordering
-    #     for X, result in data['min_fill']:
-    #         assert reasoner.ordering_min_fill(X) == result
-
-    #     # test network pruning
-    #     for Q, e, vars, edges in data['net_prune']:
-    #         temp_bn = copy.deepcopy(reasoner.bn)
-    #         assert isinstance(temp_bn, BayesNet)
-
-    #         e_list: List[Tuple] = list(tuple(x) for x in e)
-    #         edge_list: List[Tuple] = list(tuple(x) for x in edges)
-            
-    #         temp_bn.net_prune(set(Q), e)
-    #         # verify whether the pruned network has the same variables as the computed test data
-    #         assert temp_bn.get_all_variables() == vars
-    #         assert temp_bn.get_all_edges() == edge_list
-
-    #     # test marginal distributions
-    #     for Q, e, pi, result in data['marginal_distrib']:
-    #         e_list: List[Tuple] = list(tuple(x) for x in e)
-
-    #         # the output CPT from the marginal distribution
-    #         df = reasoner.marginal_dist(Q, e_list, pi)  # type: ignore
-
-    #         # the last row, which is where all variables are true
-    #         output = df.iloc[-1].loc['p']
-
-    #         result_float = float(result[0])
-
-    #         # test whether the probability value of the last row is within a delta margin of the test value
-    #         assert (result_float - DELTA) < output < (result_float + DELTA)
-
-    #     # test MAP and MPE
-    #     for _ in data['map_and_mpe']:
-    #         pass
